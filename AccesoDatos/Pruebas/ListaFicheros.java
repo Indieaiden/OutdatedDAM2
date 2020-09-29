7ba8d4cb15130c5e79fd6a36779f17d93c0a7f06
Jorge Aiden <jorgeaiden.martin@gmail.com>
2020-09-29 14:23:47 +0200

    Añadido primer ejemplo

---
+++ AccesoDatos/Pruebas/ListaFicheros.java
@@ -0,0 +1,15 @@
+package AccesoDatos.Pruebas;
+// No me acuerdo de programar en Java pero oye para alante.
+
+import java.io.File;
+
+public class ListaFicheros {
+
+    public ListaFicheros() {
+        File cuchara = new File("null");
+    }
+
+    public static void main(String[] args){
+        ListaFicheros start = new ListaFicheros();
+    }
+}
