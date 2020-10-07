993e5cb764d8bf58d729d751762135ad4012b040
Jorge Aiden <jorgeaiden.martin@gmail.com>
2020-10-07 14:14:44 +0200

    add ADEjercicios Corregidos
    Añadidos los ejercicios de la primera semana corregidos, sin probar.

---
+++ AccesoDatos/Tema1/Ejercicios_corregidos/ManejadorFichero.java
@@ -0,0 +1,80 @@
+package AccesoDatos.Tema1.Ejercicios_corregidos;
+import java.io.*;
+
+public class ManejadorFichero {
+    
+    private File f;
+
+    public ManejadorFichero()
+    {
+
+    }
+
+    /**
+     * EJERCICIO1
+     * @param nombreFichero
+     * @param dir
+     * @return
+     */
+    public boolean estaEn(String nombreFichero, String dir) {
+        boolean resultado = false;
+        //Esto está puesto así para que coja un nullpointer
+        try {
+            this.f = new File(dir, nombreFichero);
+            resultado = f.exists();
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println("No vale null");            
+        }
+        return resultado;
+    }
+
+    /**
+     * EJERCICIO 2
+     * @param directorio
+     * @return
+     * 
+     * Saco el listado 
+     */
+    public void mostrarDirect(String directorio){
+        File f=new File(directorio);
+        String[] array = f.list();
+        for (String string : array) {
+            System.out.println(string);
+        }
+    }
+
+    /**
+     * EJERCICIO3 
+     */
+
+    public boolean esDirectorio(String nombreFichero){
+            boolean resultado=false;
+            f=new File(nombreFichero);
+            if (f.isDirectory()) {
+                resultado = true;
+                this.mostrarDirect(nombreFichero);
+            }
+            return resultado;
+    }
+
+    /**
+     * EJERCICIO4
+     */
+    public void infoFichero(String fichero, String directorioActual){
+        if (this.estaEn(fichero, directorioActual)) {
+            File f = new File(fichero);
+            System.out.println("Nombre: "+f.getName());
+            System.out.println("Ruta: "+f.getAbsolutePath());
+            System.out.println("Leer: "+f.canRead());
+            System.out.println("Tamaño: "+f.getTotalSpace());
+            if (this.esDirectorio(fichero)) {
+                System.out.println("Es un directorio");
+            } else {
+                System.out.println("Es un fichero");
+            }
+        } else {
+            System.out.println("No existe aquí");
+        }
+    }
+}
---
+++ AccesoDatos/Tema1/Ejercicios_corregidos/TestManejadorFichero.java
@@ -0,0 +1,8 @@
+package AccesoDatos.Tema1.Ejercicios_corregidos;
+
+public class TestManejadorFichero {
+    public static void main(String[] args) {
+        ManejadorFichero m1 = new ManejadorFichero();
+        
+    }
+}
