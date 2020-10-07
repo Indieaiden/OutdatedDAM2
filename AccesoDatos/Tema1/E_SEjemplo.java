d3f2654ecf7550159362c49f664c1081d7a66503
Jorge Aiden <jorgeaiden.martin@gmail.com>
2020-10-07 14:24:43 +0200

    Create S_Ejemplo.java & fix a bug

---
+++ AccesoDatos/Tema1/E_SEjemplo.java
@@ -0,0 +1,24 @@
+package AccesoDatos.Tema1;
+import java.io.*;
+
+public class E_SEjemplo {
+    public void E_SEjemplo(){
+        try{
+            BufferedReader reader = 
+                new BufferedReader(new FileReader("nombreFichero"));
+                String linea = reader.readLine();
+            while(linea != null) {
+                //Procesar el texto de la linea
+        
+        linea = reader.readLine();
+            }
+            reader.close();
+            }
+        catch(FileNotFoundException e) {
+            //NoSeEncuentraFichero
+        }
+        catch(IOException e){
+            //Algo fue mal al leer o cerrar
+        }
+    }
+}
