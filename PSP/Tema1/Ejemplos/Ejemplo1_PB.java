5f343091e634ab10e6a8c9b584961d53c8984a95
Jorge Aiden <jorgeaiden.martin@gmail.com>
2020-10-09 10:34:25 +0200

    PSP-add Ejemplo con ProcessBuilder

---
+++ PSP/Tema1/Ejemplos/Ejemplo1_PB.java
@@ -0,0 +1,40 @@
+package PSP.Tema1.Ejemplos;
+import java.io.*;
+
+public class Ejemplo1_PB {
+    
+	public static void main(String[] args) { 
+       
+		String comando = "cmd"; 
+
+		try{ 
+            
+            ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "dir");
+            Process process = pb.start();
+            //Creamos un buffered Reader para poder leer línea a línea
+            //Para ello usamos el ISR en el new
+            InputStreamReader isr = new InputStreamReader(process.getInputStream());
+            try{
+                BufferedReader reader = 
+                new BufferedReader(isr);
+                    String linea = reader.readLine();
+                while(linea != null) {
+                    System.out.println(linea);
+            
+            linea = reader.readLine();
+                }
+                reader.close();
+                }
+            catch(FileNotFoundException e) {
+                //NoSeEncuentraFichero
+            }
+            catch(IOException e){
+                //Algo fue mal al leer o cerrar
+            }
+
+		}catch(Exception e){ 
+			System.out.println("ERROR EN "+comando); 
+			e.printStackTrace(); 
+		} 
+	} 
+}
